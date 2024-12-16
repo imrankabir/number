791348b12144e4779d1a1e8c66b0785b447fcdc8
Imran Kabir <imran.kabir.k@gmail.com>
2024-12-16 23:02:13 +0500

    number auto commit on 2024-12-16 11:02:13 PM

---
+++ script.js
@@ -0,0 +1,17 @@
+const container = document.querySelector('#container');
+
+const speak = n => {
+    const utter = new SpeechSynthesisUtterance(n);
+    utter.lang = 'en-US';
+    window.speechSynthesis.speak(utter);
+};
+
+for (let i = 1; i <= 100; i++) {
+    const box = document.createElement('div');
+    box.className = 'box';
+    box.textContent = i;
+    box.onclick = e => speak(i);
+    container.appendChild(box);
+}
+
+const audio = document.querySelector('#audio');
