1a7c9e4237e6bf2e976406542d26f683f64f0347
Imran Kabir <imran.kabir.k@gmail.com>
2025-01-12 14:59:38 +0500

    number auto commit on 2025-01-12 02:59:38 PM

--- script.js
+++ script.js
@@ -1,17 +1,36 @@
-const container = document.querySelector('.container');
+let num = 1;
+let timer = null;
+const boxes = [];
+const total = 100;
 
-const speak = n => {
+const speak = (n, e) => {
     const utter = new SpeechSynthesisUtterance(n);
     utter.lang = 'en-US';
     window.speechSynthesis.speak(utter);
+    e.classList.add('shake');
+    utter.onend = () => e.classList.remove('shake');
+};
+
+const start = n => {
+    clearTimeout(timer);
+    num = n;
+    play();
+};
+
+const play = e => {
+    speak(num, boxes[num - 1]);
+    num = num < total ? ++num : 1;
+    timer = setTimeout(play, 2500);
 };
 
 document.addEventListener('DOMContentLoaded', e => {
-    for (let i = 1; i <= 100; i++) {
+    const container = document.querySelector('.container');
+    for (let i = 1; i <= total; i++) {
         const box = document.createElement('div');
         box.className = 'box';
         box.textContent = i;
-        box.onclick = e => speak(i);
+        box.onclick = e => start(i);
+        boxes.push(box);
         container.appendChild(box);
     }
 });
